e6482c1bfa4291eb8386f833f0cf160d88c9c47e
xvoorvaa <xander@xvproductions.com>
2023-12-09 13:09:34 -0500

    Nuxt now renders client-side

--- components/ForceGraph.vue
+++ components/ForceGraph.vue
@@ -1,21 +1,25 @@
 <script lang="ts">
-  import ForceGraph3D from 'force-graph'
+import ForceGraph3D from 'force-graph';
 
-  const g = ForceGraph3D()(document.getElementById('graph')!)
-  const N = 300;
-  const gData = {
-    nodes: [...Array(N).keys()].map(i => ({ id: i })),
-    links: [...Array(N).keys()]
-    .filter(id => id)
-    .map(id => ({
-      source: id,
-      target: Math.round(Math.random() * (id-1))
-    }))
-  };
+export default {
+  mounted() {
+    const g = ForceGraph3D()(this.$refs.graph);
+    const N = 300;
+    const gData = {
+      nodes: [...Array(N).keys()].map(i => ({ id: i })),
+      links: [...Array(N).keys()]
+        .filter(id => id)
+        .map(id => ({
+          source: id,
+          target: Math.round(Math.random() * (id-1))
+        }))
+    };
 
-  g.graphData(gData);
+    g.graphData(gData);
+  }
+};
 </script>
 
 <template>
-  <div id="graph"></div>
+  <div ref="graph"></div>
 </template>
